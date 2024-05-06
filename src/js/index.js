4677417c22358f92068942d0a0840a1244c3f497
LoneBeast-hub <yusufoyindamola5@gmail.com>
2024-05-06 01:39:08 +0100

    modified the owner image, brands heading and added submission modal

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+// variables
+const submitForm = document.getElementById('submitForm');
+const formSubmissionModal = document.getElementById('formSubmissionModal');
+const cancelModal = document.getElementById('cancelModal');
+
+// functions
+const handleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log('submitted');
+    formSubmissionModal.style.display = 'flex';
+}
+
+const handleCancelModal = () => {
+    formSubmissionModal.style.display = 'none';
+}
+
+// event listeners
+submitForm.addEventListener('submit', handleSubmitForm);
+cancelModal.addEventListener('click', handleCancelModal);

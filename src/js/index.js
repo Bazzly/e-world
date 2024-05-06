ce604768015fb1d713063299093f74ccaf7f333a
unknown <bazeet4love@gmail.com>
2024-05-06 11:49:20 +0100

    change about color to white and expand on the about me

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,31 @@ const handleSubmitForm = (e) => {
     e.target.reset();
 }
 
+// email validation 
+
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+  
+  const validate = () => {
+    const $result = $('#result');
+    const email = $('#email').val();
+    $result.text('');
+  
+    if(validateEmail(email)){
+      $result.text(email + ' is valid.');
+      $result.css('color', 'green');
+    } else{
+      $result.text(email + ' is invalid.');
+      $result.css('color', 'red');
+    }
+    return false;
+  }
+  
+  $('#email').on('input', validate);
+
 const handleCancelModal = () => {
     formSubmissionModal.style.display = 'none';
 }
